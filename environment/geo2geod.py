97b44ad2f7ee2b4836ad5e53de29d8746460dce1
stourgai <tourgaidis.st@gmail.com>
2022-05-29 17:33:56 +0300

    Create geo2geod.py

---
+++ environment/geo2geod.py
@@ -0,0 +1,5 @@
+def geo2geod(lat_geo_phi, lon_geo_lmd, alt_geo):
+    lat_geod_phi = geo_lat2geod_lat(lat_geo_phi)  # degrees
+    lon_geod_lmd = lon_geo_lmd  # degrees
+    alt_geod = alt_geo  # km
+    return lat_geod_phi, lon_geod_lmd, alt_geod

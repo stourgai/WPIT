fda1a0b7b5589c1862717e2e0603eeda58c705c4
stourgai <tourgaidis.st@gmail.com>
2022-05-29 18:41:02 +0300

    commit jupyter first try

---
+++ environment/const.py
@@ -0,0 +1,17 @@
+Re=6378137   #Earth mean radius in m
+c_light=2.997956376932163e8 #speed of light in m/s
+me=9.10938291e-31   #electron mass in kg
+qe=1.602176565e-19 #electron charge in C
+qe_Tao=-1.602176565e-19 #electron charge in C
+qi=1.602176565e-19 #electron charge in C
+# m_O=2.6566962e-26  #oxygen mass in kg
+mO=2.67616E-026  #oxygen mass in kg
+# m_H=1.6735575e-27 #hydrogen mass in kg
+mH=1.6726E-027 #hydrogen mass in kg
+# m_He=6.6464731e-27 #helium mass in kg
+mHe=6.6904E-027 #helium mass in kg
+e_el=5.105396765648739e5   #511keV
+epsilon0=8.854187817e-12
+eps0=8.854187817e-12
+ms=[me,mH,mHe,mO]  #vector of masses
+qs=[qe,qi,qi,qi]    #vector of charges

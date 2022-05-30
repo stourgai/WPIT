520c9db3dff5447e8317bb0055fdf94d5c4ff7c6
stourgai <tourgaidis.st@gmail.com>
2022-05-30 21:32:48 +0300

    Update environment_mod.py

--- environment/environment_mod.py
+++ environment/environment_mod.py
@@ -331,3 +331,9 @@ def carpender_anderson(Lsh,Kpmax,day,mlt,Rb):
         ne_eq=trough(ne_Lppo,Lsh,L_ppo)
         
     return ne_eq
+
+def densities_ozhogin(L_arg,lambda_arg):
+    neqtmp=10**(4.4693-0.4903*L_arg)
+    lamda_inv=np.arccos(1/L_arg)
+    n_lamtmp=neqtmp*((np.cos((np.pi/2)*(lambda_arg/lamda_inv)))**(-0.75))
+    return neqtmp,n_lamtmp

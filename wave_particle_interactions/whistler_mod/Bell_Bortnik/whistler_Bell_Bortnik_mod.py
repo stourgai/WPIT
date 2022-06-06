1173b0839ac607dccfb124d71e7fc2e632e53b81
stourgai <tourgaidis.st@gmail.com>
2022-06-06 14:45:34 +0300

    new

--- wave_particle_interactions/whistler_mod/Bell_Bortnik/whistler_Bell_Bortnik_mod.py
+++ wave_particle_interactions/whistler_mod/Bell_Bortnik/whistler_Bell_Bortnik_mod.py
@@ -40,7 +40,7 @@ def p_par_rk_bort(pper_arg,eta_arg,wtau_sq_arg,kz_arg,gamma_arg,wce_arg,dwds_arg
     return lrk    
 
 def p_per_rk_bort(ppar_arg,pper_arg,eta_arg,w1_arg,w2_arg,beta_arg,gamma_arg,R1_arg,R2_arg,m_res_arg,wce_arg,dwds_arg):
-    mrk=(-(w1_arg*((ppar_arg/gamma_arg)+(const.me*R1_arg))*jn((m_res_arg-1),beta_arg)-
+    mrk=(-(pow((-1),(m_res_arg-1))*(w1_arg*((ppar_arg/gamma_arg)+(const.me*R1_arg))*jn((m_res_arg-1),beta_arg)-
             w2_arg*((ppar_arg/gamma_arg)-(const.me*R2_arg))*jn((m_res_arg+1),beta_arg))*np.sin(eta_arg)+
             ((1/(const.me*gamma_arg))*(pper_arg*ppar_arg)/(2*wce_arg))*dwds_arg)
     return mrk
